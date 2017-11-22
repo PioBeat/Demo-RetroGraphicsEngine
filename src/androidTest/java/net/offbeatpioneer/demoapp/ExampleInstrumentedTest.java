ee53ed0b447e8034efc9245695c17f28c5f7195f
Dominik Grzelak <dominik@offbeat-pioneer.net>
2017-11-22 13:14:04 +0100

    removed sprite gird test

--- src/androidTest/java/net/offbeatpioneer/demoapp/ExampleInstrumentedTest.java
+++ src/androidTest/java/net/offbeatpioneer/demoapp/ExampleInstrumentedTest.java
@@ -5,11 +5,9 @@ import android.graphics.PointF;
 import android.graphics.RectF;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.DisplayMetrics;
 
 import net.offbeatpioneer.demoapp.retrographicsengine.sprites.Mine;
 import net.offbeatpioneer.retroengine.core.RetroEngine;
-import net.offbeatpioneer.retroengine.core.sprites.SpriteGridGroup;
 import net.offbeatpioneer.retroengine.core.sprites.SpriteListGroup;
 import net.offbeatpioneer.retroengine.core.util.MathUtils;
 
@@ -63,32 +61,5 @@ public class ExampleInstrumentedTest {
 
         group1.removeInActive();
         group1.updateLogic();
-    }
-
-    @Test
-    public void gridtest() throws Exception {
-        SpriteGridGroup gridGroup = new SpriteGridGroup();
-        SpriteGridGroup gridGroup2 = new SpriteGridGroup();
-
-        Mine m = new Mine();
-        for (int i = 0; i < 11; i++) {
-            m = new Mine();
-            m.setAutoDestroy(false);
-            m.setActive(true);
-            m.setPosition(new PointF(Math.round(Math.random() * 100), Math.round(Math.random() * 100)));
-            if (i % 2 == 0) {
-                m.setActive(false);
-                m.setPosition(new PointF(-Math.round(Math.random()*100), -Math.round(Math.random()*100)));
-                gridGroup2.add(m);
-            } else {
-                gridGroup.add(m);
-            }
-            System.out.println(m.getPosition().x + "," + m.getPosition().y);
-        }
-        gridGroup.add(gridGroup2);
-
-        gridGroup.setQueryRange(new RectF(0, 0, 100, 100));
-        gridGroup.removeInActive();
-        gridGroup.updateLogic();
     }
 }
